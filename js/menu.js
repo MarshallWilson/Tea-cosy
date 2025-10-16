3834e36d0cbfac05a52cd23ed1fdad70c7c73b7a
Marshall Wilson <20324281+MarshallWilson@users.noreply.github.com>
2025-10-16 17:24:06 -0400

    Implement navigation menu toggle functionality

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+/*function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menu-toggle');
+  const navLinks = document.getElementById('nav-links');
+
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+});
